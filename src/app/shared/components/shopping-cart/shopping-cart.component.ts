cdd06b86f143aeb69273eb759a1c86619735250a
OrtizEche <ortiz.eche@gmail.com>
2022-05-23 17:26:17 -0500

    Se realiza el funcionamiento general del carrito de compras, desde agregar hasta eliminar

--- src/app/shared/components/shopping-cart/shopping-cart.component.ts
+++ src/app/shared/components/shopping-cart/shopping-cart.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { DataTransferService } from '../../services/data-transfer.service';
-import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 
 
 @Component({
@@ -8,15 +8,48 @@ import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
   templateUrl: './shopping-cart.component.html',
   styleUrls: ['./shopping-cart.component.css']
 })
-export class ShoppingCartComponent implements OnInit {
-  
-  
-  
-  @Input() listaCompra:Array<any>=[]
 
-  constructor(private transfer: DataTransferService, public activeModal: NgbActiveModal) { }
+export class ShoppingCartComponent implements OnInit {
+
+
+
+  @Input() listaCompra: Array<any> = []
+  listaUnica: Array<any> = []
+  count: number = 0
+
+  constructor(private transfer: DataTransferService) { }
 
   ngOnInit(): void {
+    this.addToCarrito()
   }
 
+  addToCarrito() {
+    this.transfer.productsInCarrito.subscribe(data => {
+      this.insertOrUpdate(data.data)
+
+    })
+  }
+
+  insertOrUpdate(elemento: any) {
+    const i = this.listaCompra.findIndex(e => e.title === elemento.title);
+    if (i > -1) {
+      this.listaCompra[i] = elemento;
+      elemento.id++
+    }
+    else this.listaCompra.push(elemento);
+
+  }
+
+  deleteElement(elemento: any) {
+    const i = this.listaCompra.findIndex(e => e.title === elemento.title);
+    if (i > -1) {
+      elemento.id--
+    }
+  }
+
+
+  cleanAll() {
+    this.listaCompra.splice(0, this.listaCompra.length)
+  }
 }
+
